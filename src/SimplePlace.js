ec01947b6c5274eec6ca3efe22d132c569a0cbc2
akjr70150 <a.rossinen@gmal.com>
2019-10-28 14:54:07 +0200

    fixed marker

--- src/SimplePlace.js
+++ src/SimplePlace.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, propTypes} from 'react';
+//import React, { Component, propTypes } from 'react/addOns';
+import { GoogleMap, withScriptjs, withGoogleMap, Marker, Map, GooleApiWrapper } from 'react-google-maps';
 
-const K_WIDTH = 40;
-const K_HEIGHT = 40;
+
+const K_WIDTH = 20;
+const K_HEIGHT = 20;
 
 const simplePlaceStyle = {
   // initially any map object has left top corner at lat lng coordinates
   // it's on you to set object origin to 0,0 coordinates
-  position: 'absolute',
+  //position: 'absolute',
   width: K_WIDTH,
   height: K_HEIGHT,
   left: -K_WIDTH / 2,
   top: -K_HEIGHT / 2,
-
-  border: '5px solid #f44336',
+  border: '5px solid #FFFFFF',
   borderRadius: K_HEIGHT,
   backgroundColor: 'white',
   textAlign: 'center',
@@ -23,19 +25,34 @@ const simplePlaceStyle = {
 };
 
 class SimplePlace extends Component {
-  static propTypes = {
-    text: 'ok'
-  };
+  center = (75.1699, 24.94);
+  markers = (1);
 
-  static defaultProps = {};
+   propTypes = {
+    text: 'ok',
+  }; 
+
+  //static defaultProps = {
+    //center:(60.1699, 24.94)
+  //};
 
   render() {
+    
     return (
+      <div>
+      
        <div style={simplePlaceStyle}>
           {this.props.text}
        </div>
-    );
-  }
-};
+       </div>
+    )
+    };
+  };
 
+
+// SimplePlace.propTypes = {
+//     //text: propTypes.string,
+//     //markers: propTypes.number,
+//     //center: propTypes,
+// }
 export default SimplePlace;
