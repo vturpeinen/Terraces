4e5835d2d903d0054079b34fbee4ceb65675c331
Antti Rossinen <43367616+akjr70150@users.noreply.github.com>
2019-10-28 15:25:33 +0200

    Update SimplePlaceContainer.js

--- src/SimplePlaceContainer.js
+++ src/SimplePlaceContainer.js
@@ -4,7 +4,7 @@ import  SimplePlace from './SimplePlace';
 class SimplePlaceList extends Component {
   render() {
     const places = this.props.places.map((item) =>
-      <SimplePlace lat={item.lat} lng={item.lng} text={'Toimi SAATANA'} /* road circle */ />
+      <SimplePlace lat={item.lat} lng={item.lng} text={''} /* road circle */ />
     );
     return (
       <div>
